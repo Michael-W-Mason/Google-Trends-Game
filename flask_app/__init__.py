0421cfb28e3a3808a618d1fa789b1c75d0999e5e
Michael Mason <mwmason1998@gmail.com>
2022-06-29 14:29:23 -0400

    stat

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 
 # Creates an instance of our app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/trendy/static")
 CORS(app)
 
 # Used for sessions
